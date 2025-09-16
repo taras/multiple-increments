151a261a1e58dc214bec446a796ce8f2c58493fe
Taras Mankovski <taras@frontside.com>
2025-09-16 04:12:57 +0200

    Use @effectionx/signals package

--- number.ts
+++ number.ts
@@ -1,30 +1,5 @@
-import { createSignal, type Operation, resource, type Stream } from "effection";;
-
-/**
- * @source: https://github.com/thefrontside/effectionx/blob/main/signals/types.ts
- * 
- * A signal is a stream with set, update, and valueOf methods.
- * Subscribing to a signal will yield the current value of the signal.
- */
-export interface ValueSignal<T> extends Stream<T, void> {
-  /**
-   * Set the value of the signal.
-   * @param value - The value to set the signal to.
-   * @returns The value of the signal.
-   */
-  set(value: T): T;
-  /**
-   * Update the value of the signal.
-   * @param updater - The updater function.
-   * @returns The value of the signal.
-   */
-  update(updater: (value: T) => T): T;
-  /**
-   * Get the current value of the signal.
-   * @returns The current value of the signal.
-   */
-  valueOf(): Readonly<T>;
-}
+import { createSignal, type Operation, resource } from "effection";
+import type { ValueSignal } from '@effectionx/signals';
 
 export interface NumberSignal extends ValueSignal<number> {}
 
